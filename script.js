eeee99df4ba30e2757be157cf511dc8ca16df172
Adriel <adrielladeira01@gmail.com>
2024-09-11 22:26:04 -0300

    function cadastrar meta

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-const start = () => {
+const { select, input } = require('@inquirer/prompts')
+
+const cadastrarMeta = async () => {
+    const meta = await input ({message: "Digite a meta:"})
+
+    if( meta.length == 0){
+        console.log('A meta não pode ser vazia')
+        return
+    }
+}   
+const start = async() => {
+
     while (true){
-        let opção =
-        switch () {
-            case "cadastrar";
+
+      const opcao = await select({
+         message: "Menu >",
+         choices: [{
+            name: "Cadastrar meta",
+            value: "Cadastrar"
+         },
+         {
+            name: "Listar metas",
+            value: "listar"
+         },
+         {
+            name: "Sair",
+            value: "sair"
+         }]
+       })
+
+        switch (opcao) {
+            case "cadastrar":
+            await cadastrarMeta ()
+                break
+            case "listar":
 
                 break
-            case "listar";
+            case "sair":
 
-                break
-            case "sair";
-
-                break
+                return
         }
 
     }
